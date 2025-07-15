3270e9ef3ed87679cd0c79b17c5e421578f87ffa
tmp0810 <truongminhphuc08102005@gmail.com>
2025-07-15 23:11:31 +0700

    Update scitail_moe.sh

--- scripts/SentencePair/kd/scitail/scitail_moe.sh
+++ scripts/SentencePair/kd/scitail/scitail_moe.sh
@@ -101,7 +101,7 @@ export NCCL_DEBUG=""
 export WANDB_DISABLED=True
 export TF_CPP_MIN_LOG_LEVEL=3
 export PYTHONPATH=${BASE_PATH}
-CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/SentencePair/distillation.py ${OPTS}"
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/SentencePair/MoE_distillation.py ${OPTS}"
 
 ${CMD} \
 >> ${SAVE_PATH}/train.log 2>&1 &
