c83a2329a7da5221459edb607a0f44a3e6eca766
tmp0810 <truongminhphuc08102005@gmail.com>
2025-07-18 15:07:55 +0700

    Update __init__.py

--- STS/criterions/__init__.py
+++ STS/criterions/__init__.py
@@ -4,13 +4,15 @@ from .rmse_cka import RMSE_CKA
 from .ot_pro import OT_PRO
 from .ot_pro_rmse_cka import OT_PRO_RMSE_CKA
 from .min_cka import MIN_CKA
+from .moe_sts_loss import MoE_STSLoss
 criterion_list = {
     "sts_loss": STSLoss,
     "dual_space_kd_with_cross_model_attention": DualSpaceKDWithCMA,
     "rmse_cka": RMSE_CKA,
     "ot_pro": OT_PRO,
     "ot_pro_rmse_cka": OT_PRO_RMSE_CKA,
-    "min_cka": MIN_CKA
+    "min_cka": MIN_CKA,
+    "moe_sts_loss": MoE_STSLoss
 }
 
 def build_criterion(args):
