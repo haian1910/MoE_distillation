143be3c28be9a14e2f3fa1feac2db8ca9baa38fa
tmp0810 <truongminhphuc08102005@gmail.com>
2025-07-22 00:22:41 +0700

    Update arguments_2teacher.py

--- Classification/arguments_2teacher.py
+++ Classification/arguments_2teacher.py
@@ -17,7 +17,7 @@ import argparse
 import os
 import deepspeed
 import numpy as np
-from SentencePair.distiller import Distiller
+from Classification.distiller import Distiller
 
 
 def add_model_args(parser: argparse.ArgumentParser):
