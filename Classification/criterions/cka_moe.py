7c31136cda576373bf569af72eb6631a4070bce7
tmp0810 <truongminhphuc08102005@gmail.com>
2025-08-26 10:48:23 +0700

    Update cka_moe.py

--- Classification/criterions/cka_moe.py
+++ Classification/criterions/cka_moe.py
@@ -139,7 +139,7 @@ class CKA_MOE(CrossEntropyLossMoE):
         print("topk_cka_loss:", topk_cka_loss)
 
         # Final loss combination
-        loss = (1.0 - self.kd_rate) * loss + self.kd_rate * (0.8*total_moe_loss + 0.2*topk_cka_loss)
+        loss = (1.0 - self.kd_rate) * loss + self.kd_rate * (0.9*total_moe_loss + 0.1*topk_cka_loss)
         log["loss"] = loss.detach().clone()  # Store as tensor for distributed logging
 
         # Compute accuracy
