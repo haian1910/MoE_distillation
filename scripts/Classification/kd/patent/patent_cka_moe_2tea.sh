1e37545af285d928a24ce2d3bec3429c4074cf4a
tmp0810 <truongminhphuc08102005@gmail.com>
2025-09-04 14:47:26 +0700

    Update patent_cka_moe_2tea.sh

--- scripts/Classification/kd/patent/patent_cka_moe_2tea.sh
+++ scripts/Classification/kd/patent/patent_cka_moe_2tea.sh
@@ -16,7 +16,7 @@ DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
 
 # model
 BASE_PATH=/content/MoE_distillation
-CKPT_NAME="bert"
+CKPT_NAME="bert_patent_2tea"
 CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
 TEACHER_MODEL_PATH="/content/drive/MyDrive/Colab_Notebooks/data_distillation/checkpoint_for_test/Llama_sft_patent" # checkpoint LLM2Vec Mistral 7B
